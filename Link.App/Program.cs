34ea155eeeab0db55f6f65637a940e0beff804f6
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-07 22:32:32 -0500

    Added Link.App with FxCop

---
+++ Link.App/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Link.App
+{
+    internal sealed class Program
+    {
+        private static void Main() => Console.WriteLine("Hello Link!");
+    }
+}

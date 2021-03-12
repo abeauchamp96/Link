0e955f8dc1acd847a202a82c349df7bba718fec9
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-12 15:20:15 -0500

    Added Hosting to start the application

--- Link.App/Program.cs
+++ Link.App/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
 // Licensed under the MIT license.
 
-using System;
+using System.Threading.Tasks;
 
 namespace Link.App
 {
     internal sealed class Program
     {
-        private static void Main() => Console.WriteLine("Hello Link!");
+        private static async Task Main() => await Startup.RunAsync();
     }
 }
---
+++ Link.App/Startup.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Link.App
+{
+    public static class Startup
+    {
+        public static async Task RunAsync()
+        {
+            using var host = BuildHost();
+
+            await host.RunAsync();
+
+            static IHost BuildHost()
+                => Host.CreateDefaultBuilder(Array.Empty<string>())
+                    .Build();
+        }
+    }
+}

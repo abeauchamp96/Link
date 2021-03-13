8efb82c70d7d2a31fc23881f3a5bb7d466db7185
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-13 17:56:00 -0500

    Added helper for HostApplicationLifetime

---
+++ Link.App/Helpers/HostLifetimeHelper.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Link.App.Helpers
+{
+    internal sealed class HostLifetimeHelper : IHostLifetimeHelper
+    {
+        private readonly IHostApplicationLifetime hostApplicationLifetime;
+
+        public HostLifetimeHelper(IHostApplicationLifetime hostApplicationLifetime)
+            => this.hostApplicationLifetime = hostApplicationLifetime;
+
+        public void RegisterApplicationStarted(Action callback)
+            => this.hostApplicationLifetime.ApplicationStarted.Register(callback);
+
+        public void RegisterApplicationStopped(Action callback)
+            => this.hostApplicationLifetime.ApplicationStopped.Register(callback);
+
+        public void RegisterApplicationStopping(Action callback)
+            => this.hostApplicationLifetime.ApplicationStopping.Register(callback);
+    }
+}
---
+++ Link.App/Helpers/IHostLifetimeHelper.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Link.App.Helpers
+{
+    public interface IHostLifetimeHelper
+    {
+        void RegisterApplicationStarted(Action callback);
+        void RegisterApplicationStopped(Action callback);
+        void RegisterApplicationStopping(Action callback);
+    }
+}

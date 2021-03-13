d5dea5ff1b773bbbe399e48811dc4acc6cfe2f5c
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-13 11:23:45 -0500

    Added appsettings

---
+++ Link.App/Settings/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Link.App.Settings
+{
+    internal sealed record AppSettings
+    {
+        public string Name { get; init; } = string.Empty;
+
+        public string Version { get; init; } = string.Empty;
+    }
+}
--- Link.App/Startup.cs
+++ Link.App/Startup.cs
@@ -1,23 +1,41 @@
 ﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
 // Licensed under the MIT license.
 
+using Link.App.Settings;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Threading.Tasks;
 
 namespace Link.App
 {
-    public static class Startup
+    internal static class Startup
     {
         public static async Task RunAsync()
         {
             using var host = BuildHost();
 
+            var options = host.Services.GetRequiredService<IOptions<AppSettings>>();
+
+            Console.WriteLine(options.Value);
+
             await host.RunAsync();
 
             static IHost BuildHost()
-                => Host.CreateDefaultBuilder(Array.Empty<string>())
+            {
+                return Host
+                    .CreateDefaultBuilder(Array.Empty<string>())
+                    .ConfigureServices(ConfigureServices)
                     .Build();
+
+                static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+                {
+                    services.Configure<AppSettings>(host.Configuration.GetSection("App"));
+
+                    services.Configure<ConsoleLifetimeOptions>(o => o.SuppressStatusMessages = true);
+                }
+            }
         }
     }
 }

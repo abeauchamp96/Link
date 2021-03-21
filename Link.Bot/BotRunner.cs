e413113a2cc2d4b61b9d8604c87fb5d07a335d36
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-21 00:22:31 -0400

    Added empty BotRunner class

---
+++ Link.Bot/BotRunner.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Link.Bot
+{
+    public class BotRunner : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken) => throw new System.NotImplementedException();
+    }
+}

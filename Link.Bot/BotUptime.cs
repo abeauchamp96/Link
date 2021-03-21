876b36e70a3f1786d2264270406d2d9fe2e132ad
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-21 00:49:51 -0400

    Added Uptime

---
+++ Link.Bot/BotUptime.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Link.Bot
+{
+    internal sealed record BotUptime(DateTimeOffset ElapsedDateTime) : IUptime;
+}
---
+++ Link.Bot/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Link.Bot.Tests")]
+namespace Link.Bot
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureBot(this IServiceCollection services)
+            => services.AddSingleton<IUptime>(s => new BotUptime(DateTimeOffset.Now));
+    }
+}

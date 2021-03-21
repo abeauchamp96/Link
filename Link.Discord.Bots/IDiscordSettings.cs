e2422537c9cf4290c9defae3673fce9f08d40c0b
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-21 16:37:23 -0400

    Added Discord settings

---
+++ Link.Discord.Bots/IDiscordSettings.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Link.Discord.Bots.Models;
+
+namespace Link.Discord.Bots
+{
+    public interface IDiscordSettings
+    {
+        int MessageCacheSize { get; }
+        DiscordLogLevel LogLevel { get; }
+        bool CaseSensitiveCommands { get; }
+        char ArgumentSeparator { get; }
+        DiscordCommandRunMode CommandRunMode { get; }
+    }
+}

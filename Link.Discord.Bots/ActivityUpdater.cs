d8f3726a2be93f982df3e1868ec8193d7f714db0
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-28 11:26:48 -0400

    Added ActivityUpdater to update activity

---
+++ Link.Discord.Bots/ActivityUpdater.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Discord.WebSocket;
+using Link.Discord.Bots.Mappers;
+using System.Threading.Tasks;
+
+namespace Link.Discord.Bots
+{
+    internal sealed class ActivityUpdater : IActivityUpdater
+    {
+        private readonly DiscordSocketClient discordSocketClient;
+
+        public ActivityUpdater(DiscordSocketClient discordSocketClient)
+            => this.discordSocketClient = discordSocketClient;
+
+        public Task UpdateActivityAsync(DiscordActivity discordActivity)
+            => this.discordSocketClient.SetActivityAsync(discordActivity.ToActivity());
+    }
+}
---
+++ Link.Discord.Bots/IActivityUpdater.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Threading.Tasks;
+
+namespace Link.Discord.Bots
+{
+    public interface IActivityUpdater
+    {
+        Task UpdateActivityAsync(DiscordActivity discordActivity);
+    }
+}

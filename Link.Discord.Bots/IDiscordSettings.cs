a148ddae6173532c7da18ca5ab1b75e62c581e4d
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-28 11:20:19 -0400

    Updated Discord settings to have an activity for the bot

--- Link.Discord.Bots/IDiscordSettings.cs
+++ Link.Discord.Bots/IDiscordSettings.cs
@@ -7,10 +7,11 @@ namespace Link.Discord.Bots
 {
     public interface IDiscordSettings
     {
-        int MessageCacheSize { get; }
-        DiscordLogLevel LogLevel { get; }
-        bool CaseSensitiveCommands { get; }
-        char ArgumentSeparator { get; }
-        DiscordCommandRunMode CommandRunMode { get; }
+        int MessageCacheSize { get; init; }
+        DiscordLogLevel LogLevel { get; init; }
+        bool CaseSensitiveCommands { get; init; }
+        char ArgumentSeparator { get; init; }
+        DiscordCommandRunMode CommandRunMode { get; init; }
+        DiscordActivity? Activity { get; init; }
     }
 }

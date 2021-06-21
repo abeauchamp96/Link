6a66e4a4fb3dc94eb44581e3ff3c95bcbfc8cdf2
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-06-20 23:32:17 -0400

    Configure startup settings

---
+++ Link.Bot/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace Link.Bot.Extensions
+{
+    internal static class HostBuilderExtensions
+    {
+        public static IHostBuilder ConfigureAppSettings(this IHostBuilder hostBuilder)
+            => hostBuilder.ConfigureAppConfiguration(ConfigureDevelopmentSettings);
+
+        private static void ConfigureDevelopmentSettings(HostBuilderContext hostBuilderContext, IConfigurationBuilder configurationBuilder)
+        {
+            if (!hostBuilderContext.HostingEnvironment.IsDevelopment())
+                return;
+
+            var path = Path.GetFullPath("appsettings.Development.json");
+            configurationBuilder.AddJsonFile(path);
+        }
+    }
+}
--- Link.Bot/Program.cs
+++ Link.Bot/Program.cs
@@ -1,7 +1,21 @@
 // Copyright (c) Alexandre Beauchamp. All rights reserved.
 // Licensed under the MIT license.
 
+using Link.Bot.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppSettings()
+    .UseSystemd();
+
+ConfigureServices(builder);
+
 await builder.Build().RunAsync();
+
+static void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.Configure<ConsoleLifetimeOptions>(o => o.SuppressStatusMessages = true);
+}

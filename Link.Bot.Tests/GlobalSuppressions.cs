0bfc08c9be3ee7b16667cd50b52e3f0eac12fff6
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-06-20 22:55:55 -0400

    Remove all projects

--- Link.Bot.Tests/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
-// Licensed under the MIT license.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1204:Static elements should appear before instance elements", Justification = "No need in unit tests", Scope = "module")]
--- Link.Bot/BotUptime.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
-// Licensed under the MIT license.
-
-using System;
-
-namespace Link.Bot
-{
-    internal sealed record BotUptime(DateTimeOffset ElapsedDateTime) : IUptime;
-}

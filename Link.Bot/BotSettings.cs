0826be8f765c5a373e84a495e2034c19e38b6a2c
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-06-25 14:16:22 -0400

    Added a bot connector interface

---
+++ Link.Bot/BotSettings.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Link.Bot
+{
+    public record BotSettings
+    {
+        public string Name { get; init; } = string.Empty;
+        public string Token { get; init; } = string.Empty;
+        public string Version { get; init; } = string.Empty;
+        public Uri? ProjectUrl { get; init; }
+    }
+}
---
+++ Link.Bot/IBotConnector.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Link.Bot
+{
+    public interface IBotConnector
+    {
+        BotConnectionState State { get; }
+
+        void Connect(string token);
+        void Disconnect();
+    }
+}

b458b2bb57f6a3dff61f9a1f6867427830310099
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-06-25 22:45:08 -0400

    Configure discord and added Health check for the bot

--- Link.Server/Program.cs
+++ Link.Server/Program.cs
@@ -1,10 +1,16 @@
 // Copyright (c) Alexandre Beauchamp. All rights reserved.
 // Licensed under the MIT license.
 
+using Link.Bot;
+using Link.Discord.Client.Extensions;
 using Link.Server.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Pandora.Utility.Health;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +19,31 @@ builder.Host.ConfigureAppSettings()
 
 ConfigureServices(builder);
 
-await builder.Build().RunAsync();
+await using var app = builder.Build();
+
+ConfigureApp(app);
+
+await app.RunAsync();
 
 static void ConfigureServices(WebApplicationBuilder builder)
 {
-    builder.Services.Configure<ConsoleLifetimeOptions>(o => o.SuppressStatusMessages = true);
+    builder.Services.AddHealthChecks()
+        .AddCheck<BotHealthCheck>("bot", tags: new[] { "all", "bot" });
+
+    builder.Services
+        .AddDiscord(builder.Configuration.GetSection("bot:discord"));
+
+    builder.Services.Configure<ConsoleLifetimeOptions>(o => o.SuppressStatusMessages = true)
+        .Configure<BotSettings>(builder.Configuration.GetSection("bot"));
+}
+
+static void ConfigureApp(WebApplication app)
+{
+    var options = new HealthCheckOptions().UseModuleResponseWriter();
+
+    app.UseHealthChecks("/health", CreateHealthCheckOptions(h => h.Tags.Contains("all")))
+        .UseHealthChecks("/health/bot", CreateHealthCheckOptions(h => h.Tags.Contains("bot")));
+
+    static HealthCheckOptions CreateHealthCheckOptions(Func<HealthCheckRegistration, bool> predicate)
+        => new HealthCheckOptions() { Predicate = predicate }.UseModuleResponseWriter();
 }

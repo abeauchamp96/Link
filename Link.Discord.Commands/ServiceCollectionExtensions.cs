ec6a084eb76548563eee1384957e82665c7f53b0
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-06-28 22:46:13 -0400

    Added Tests for Discord commands

---
+++ Link.Discord.Commands/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexandre Beauchamp. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Link.Discord.Commands.Tests")]
+namespace Link.Discord.Commands
+{
+    public static class ServiceCollectionExtensions
+    {
+    }
+}
